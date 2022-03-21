ec94c12c6c04c58647d8a62308500466a4a58145
jakecosgrove <jake.cosgrove@hippodigital.co.uk>
2022-03-21 12:06:00 +0000

    Pelinincome - branching test

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post('/pelinincomequestion', function (req, res) {
+
+    //make a variable and give it the value from 'pelinincomequestion'
+    var Over16k = req.session.data['interview-needs']
+        // Check whether the variable matches a condition
+   if (Over16k == "No"){
+   // Send user to next page
+   res.redirect('/jake-confirmation')
+} else {
+    // Send user to ineligible page
+    res.redirect('pelinincomeevidence')
+  }
+ })
 
 module.exports = router
